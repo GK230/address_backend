cf74a8fe933bc35154785e9c4b2bc188a011547f
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-12-14 17:37:46 +0000

    connected backend to db

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.post("/add", (req, res) => {
-  console.log(req.body);
-});
-
-app.listen(4000, function () {
-  console.log("listening on 4000");
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
 });
