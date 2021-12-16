ccc48bb2e5e32c9054b3b56c4c983d45fa5f3a5e
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-12-16 13:46:59 +0000

    added contact

--- db/conn.js
+++ db/conn.js
@@ -4,22 +4,21 @@ const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
+
 var _db;
- 
+
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
       // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("myFirstDatabase");
-        console.log("Successfully connected to MongoDB."); 
+      if (db) {
+        _db = db.db("address");
+        console.log("Successfully connected to MongoDB.");
       }
       return callback(err);
-         });
+    });
   },
- 
+
   getDb: function () {
     return _db;
   },
