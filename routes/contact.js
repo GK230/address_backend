2e6b401efdcdf9d643664282218b33bf84638498
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-12-22 11:28:03 +0000

    fixed route bug

--- routes/contact.js
+++ routes/contact.js
@@ -1,17 +1,11 @@
 const express = require("express");
 
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
 const contactRoutes = express.Router();
 
-// This will help us connect to the database
 const dbo = require("../db/conn");
 
-// This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-// This section will help you get a list of all the records.
 contactRoutes.route("/contact").get(function (req, res) {
   let db_connect = dbo.getDb("address");
   db_connect
@@ -35,7 +29,6 @@ contactRoutes.route("/contact/:name").get(function (req, res) {
     });
 });
 
-// This section will help you create a new record.
 contactRoutes.route("/contact/add").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myobj = {
@@ -50,31 +43,31 @@ contactRoutes.route("/contact/add").post(function (req, response) {
   });
 });
 
-contactRoutes.route("/update/:name").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  let newvalues = {
-    $set: {
-      person_name: req.body.person_name,
-      person_position: req.body.person_position,
-      person_level: req.body.person_level,
-    },
-  };
-  db_connect
-    .collection("contacts")
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-      console.log("1 document updated");
-      response.json(res);
-    });
-});
+// contactRoutes.route("/update/:name").post(function (req, response) {
+//   let db_connect = dbo.getDb();
+//   let myquery = { _id: ObjectId(req.params.id) };
+//   let newvalues = {
+//     $set: {
+//       name: req.body.name,
+          // email: req.body.email,
+          // phone: req.body.phone,
+          // address: req.body.address,
+//     },
+//   };
+//   db_connect
+//     .collection("contacts")
+//     .updateOne(myquery, newvalues, function (err, res) {
+//       if (err) throw err;
+//       console.log("1 document updated");
+//       response.json(res);
+//     });
+// });
 
-contactRoutes.route("/:name").delete((req, response) => {
+contactRoutes.route("/contact/:name").delete((req, response) => {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
+  let myquery = { name: req.params.name };
   db_connect.collection("contacts").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
-    console.log("1 document deleted");
     response.status(obj);
   });
 });
